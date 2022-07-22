dd2f345364cac0d05cdb01ed53051b76d56b44c6
gaurav78528 <gauravsuthar309@gmail.com>
2022-07-22 07:56:53 +0530

    added files

--- index.js
+++ index.js
@@ -73,6 +73,45 @@ let logoArr = [
   },
 ];
 
+let newsArr = [
+  {
+    image:
+      "https://cdn.dnaindia.com/sites/default/files/styles/full/public/2022/07/21/2523938-2522529-2455342-draupadi-murmu.jpg",
+    news: "Droupadi Murmu's inspirational life: Two sons, husband died within four years, but she continued her march",
+    link: "LatestNews.html",
+  },
+  {
+    image:
+      "https://cdn.dnaindia.com/sites/default/files/styles/full/public/2022/07/21/2523922-sonia-rahul.jpg",
+    news: "National Herald case explained: Why is Sonia Gandhi appearing before ED today?",
+    link: "photos.html",
+  },
+  {
+    image:
+      "https://cdn.dnaindia.com/sites/default/files/styles/full/public/2022/07/21/2523947-microsoft-teams-out.png",
+    news: "Microsoft Teams outage triggers meme fest on Twitter, corporate workers call it ‘the best day’",
+    link: "videos.html",
+  },
+  {
+    image:
+      "https://cdn.dnaindia.com/sites/default/files/styles/full/public/2022/07/21/2523948-sanjay-duttpr-handout-1.jpg",
+    news: "Shamshera director Karan Malhotra recalls Sanjay Dutt battling cancer throughout film's shoot",
+    link: "explainers.html",
+  },
+  {
+    image:
+      "https://cdn.dnaindia.com/sites/default/files/styles/full/public/2022/07/21/2523911-rishi-sunak-liz-truss.jpg",
+    news: "Rishi Sunak scripts history, to face Liz Truss in final leg of British PM contest",
+    link: "india.html",
+  },
+  {
+    image:
+      "https://cdn.dnaindia.com/sites/default/files/styles/full/public/2022/07/21/2523936-sourav-ganguly-jay-shah.jpg",
+    news: "DNA Explainer: What is the BCCI constitution amendment plea? How it affects Sourav Ganguly, Jay Shah?",
+    link: "entertainment.html",
+  },
+];
+//  JS for left section
 logoArr.forEach(function (elem) {
   let links = document.createElement("a");
   links.setAttribute("href", elem.link);
@@ -86,4 +125,20 @@ logoArr.forEach(function (elem) {
   links.append(div);
   div.append(img, name);
   document.querySelector("#left-section").append(links);
+});
+
+// JS for center section
+newsArr.forEach(function (elem) {
+  let links = document.createElement("a");
+  links.setAttribute("href", elem.link);
+  // links.innerText = elem.name;
+  let div = document.createElement("div");
+  let img = document.createElement("img");
+  img.setAttribute("src", elem.image);
+
+  let news = document.createElement("p");
+  news.innerText = elem.news;
+  links.append(div);
+  div.append(img, news);
+  document.querySelector("#news").append(links);
 });
